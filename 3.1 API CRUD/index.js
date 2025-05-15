afef354cfe359c392e514122cfda5196925d3fef
Praxedis-Calleja <65052872+Praxedis-Calleja@users.noreply.github.com>
2025-05-14 22:05:41 -0500

    Entrega final

--- 3.1 API CRUD/index.js
+++ 3.1 API CRUD/index.js
@@ -3,10 +3,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import Usuario from './models/usuario.model.js';
 
-
-
-
-
 const app = express();
 const PORT = 3000;
 dotenv.config();
@@ -22,13 +18,9 @@ app.get('/', (req, res) => {
     res.send('Bienvenido a la API CRUD ');
 });
 
-
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     });
-
-
 
     mongoose.connect(uri)
     .then(() => {
@@ -38,8 +30,6 @@ app.listen(PORT, () => {
     });
 
 app.post('/usuarios', async (req, res) => {
-
-
   try {
     const usuario = await Usuario.create(req.body);
     res.status(201).json(usuario);
@@ -49,3 +39,56 @@ app.post('/usuarios', async (req, res) => {
   }
 });
 
+
+app.get("/usuarios", async (req, res) => {	
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+    
+  } catch (error) {
+    console.error('Error al obtener los usiuarios:', error);
+    res.status(500).json({error: 'Error al obtener los usiarios'});
+    
+  }
+})
+
+app.get("/usuarios/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usuario = await Usuario.findById(id);
+    res.status(200).json(usuario);
+  } catch (error) {
+     console.error('Error al obtener el id:', error);
+    res.status(500).json({error: 'Error al obtener al id'});
+  }
+})
+
+app.put("/usuarios/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+    if(!usuario) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+    const usuarioActualizado = await Usuario.findById(id);
+    res.status(200).json(usuarioActualizado);
+    console.log(usuarioActualizado);
+
+  } catch (error) {
+    console.error('Error al obtener el id:', error);
+    res.status(500).json({error: 'Error al obtener al id'});
+  }
+})
+app.delete("/usuarios/:id", async (req, res) => {
+try {
+    const {id}= req.params;
+  const usuario = await Usuario.findByIdAndDelete(id);
+  if(!usuario){
+    return res.status(400).json({error: 'Usuario no encontrado'});
+  }
+  res.status(200).json({message: 'Usuario eliminado'});
+} catch (error) {
+  console.error('Error al eliminar el usuario:', error);
+  res.status(500).json({error: 'Error al eliminar el usuario'});
+}
+})
